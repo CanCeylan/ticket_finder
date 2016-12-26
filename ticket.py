02d4f61d506f4d85635d58bca3dff5a82d2d0318
Orhan Can Ceylan <orhancanceylan@gmail.com>
2016-12-26 21:45:49 +0100

    initial commit

---
+++ ticket.py
@@ -0,0 +1,50 @@
+from lxml import html
+import requests
+import time
+import datetime
+from random import randint
+from slackclient import SlackClient
+
+SLACK_TOKEN = "xoxp-3361645106-60725169143-91094041379-72566246ce1b2fbd1dc0541b6ac4acd1"
+SLACK_CHANNEL = "#ticket-swap"
+# urls = ['https://www.ticketswap.com/event/nicolas-jaar/681833ee-39ed-457d-b9a0-682bece0bdc4', 'https://www.ticketswap.com/event/four-tet-floating-points-ade/099895ad-cd05-435b-b34b-c099f64c466d']
+urls = ['https://www.ticketswap.com/event/nicolas-jaar/681833ee-39ed-457d-b9a0-682bece0bdc4']
+queries = 0
+sc = SlackClient(SLACK_TOKEN)
+SLEEP_INTERVAL = 10
+MAX_SLEEP = 600
+
+def checkTickets(url):
+  page = requests.get(url)
+  tree = html.fromstring(page.content)
+  buyers = tree.xpath('//div[@class="counter-value"]/text()')
+  if len(buyers) > 0:
+    result = int(buyers[0])
+  else:
+    result = 0
+    time.sleep(MAX_SLEEP)
+    notifyMe("They make me sleep boss :(")
+
+  SLEEP_INTERVAL = randint(20, 36)
+  time.sleep(SLEEP_INTERVAL)
+
+  return result
+
+def notifyMe(url):
+  print "Bilet geldi abiii"
+  sc.api_call(
+  "chat.postMessage", channel=SLACK_CHANNEL, text=url,
+  username='pybot', icon_emoji=':stuck_out_tongue_winking_eye:'
+  )
+
+while True:
+
+  for u in urls:
+    if checkTickets(u) > 0:
+      notifyMe(u)
+
+  SLEEP_INTERVAL *= 20
+  queries += 1
+  taym = datetime.datetime.now() + datetime.timedelta(seconds = SLEEP_INTERVAL)
+  print "No tickets sir",queries,"th try and waking up at",taym.hour,":",taym.minute,"in",round(SLEEP_INTERVAL / 60.0, 2),"minutes..."
+  time.sleep(SLEEP_INTERVAL)
---
+++ ts.py
@@ -0,0 +1,51 @@
+from lxml import html
+import requests
+import time
+import datetime
+from random import randint
+from slackclient import SlackClient
+import os
+
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
+print SLACK_TOKEN
+
+urls = ['https://www.ticketswap.nl/event/cigarettes-after-sex/b3171998-7d0c-415a-9390-74ba51a3a38b']
+queries = 0
+sc = SlackClient(SLACK_TOKEN)
+SLEEP_INTERVAL = 10
+MAX_SLEEP = 2000
+
+def checkTickets(url):
+  page = requests.get(url)
+  tree = html.fromstring(page.content)
+  buyers = tree.xpath('//div[@class="counter-value"]/text()')
+  if len(buyers) > 0:
+    result = int(buyers[0])
+  else:
+    result = 0
+    notifyMe("They make me sleep boss :( Tell that I'm not a robot.")
+    time.sleep(MAX_SLEEP)
+
+  return result
+
+def notifyMe(text):
+  sc.api_call(
+  "chat.postMessage", channel=SLACK_CHANNEL, text=text,
+  username='Sumeyye', icon_emoji=':relieved:'
+  )
+
+while True:
+
+  for u in urls:
+    if checkTickets(u) > 0:
+      notifyMe("I FOUND A TICKET!!11!BIR!BIR1!! %s" % u)
+
+  SLEEP_INTERVAL = randint(10, 16)*20
+  queries += 1
+  taym = datetime.datetime.now() + datetime.timedelta(seconds = SLEEP_INTERVAL)
+
+  if (queries % 50 == 0 and queries > 0):
+    notifyMe("Still no tickets sir... It's my %dth try, but I will try again at %d:%d in %.2f minutes. Don't worry we will find a ticket..." % (queries,taym.hour,taym.minute,SLEEP_INTERVAL/60.0))
+
+  print "It's my %dth try, but I will try again at %d:%d in %.2f minutes. Don't worry we will find a ticket sir..." % (queries,taym.hour,taym.minute,SLEEP_INTERVAL/60.0)
+  time.sleep(SLEEP_INTERVAL)
